6387baa2afd5855b2bd7004bac0cf8f81a0c0106
elegancevns <seo@theelegance.co.in>
2026-01-06 15:38:37 +0530

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,1 +1,13 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
+
+  return null; // or a minimal loader if you want
+}
 
