e85c03ea5ba92483114caf2155709cd820ab42a1
elegancevns <seo@theelegance.co.in>
2026-01-06 15:29:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.theelegance.co.in",
+        hostname: "www.inforbit.in",
       },
     ],
   },
